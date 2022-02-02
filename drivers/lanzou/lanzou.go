4169972b0b34aa94492bbedf641a7cc798e61a50
kxsh2020 <72491044+kxsh2020@users.noreply.github.com>
2022-02-03 02:10:47 +0800

    Update lanzou.go

--- drivers/lanzou/lanzou.go
+++ drivers/lanzou/lanzou.go
@@ -222,7 +222,7 @@ func (driver *Lanzou) GetLink(downId string, account *model.Account) (string, er
 		return "", fmt.Errorf("get iframe empty page")
 	}
 	signs := ajaxdata[1]
-	sign := regexp.MustCompile(`'sign':'(.+?)';`).FindStringSubmatch(res.String())[1]
+	sign := regexp.MustCompile(`'sign':'(.+?)',`).FindStringSubmatch(res.String())[1]
 	websign := regexp.MustCompile(`var websign = '(.+?)'`).FindStringSubmatch(res.String())[1]
 	websignkey := regexp.MustCompile(`var websignkey = '(.+?)'`).FindStringSubmatch(res.String())[1]
 	var resp LanzouLinkResp
