485ead9a194710b177ff1c6825e37d56aec834a7
kxsh2020 <72491044+kxsh2020@users.noreply.github.com>
2022-02-03 10:52:03 +0800

    Update lanzou.go

--- drivers/lanzou/lanzou.go
+++ drivers/lanzou/lanzou.go
@@ -253,5 +253,5 @@ func init() {
 	base.RegisterDriver(&Lanzou{})
 	lanzouClient.
 		SetRetryCount(3).
-		SetHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
+		SetHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36")
 }
